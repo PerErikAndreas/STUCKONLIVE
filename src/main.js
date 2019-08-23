f3dac0a802ea1e34a824f06d6227d71e5ee1a42f
Winardi <winardidesign@yahoo.com>
2019-08-23 14:36:03 +0700

    First commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HelloWorld from './components/HelloWorld'
+
+Vue.component("Header", Header)
+Vue.component("Footer", Footer)
+Vue.component("HelloWorld", HelloWorld)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
