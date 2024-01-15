954ba2ff5fad9398ee59c553405dd820d6527d69
Andreas Axelsson <pererikandreas@gmail.com>
2024-01-15 20:29:26 +0100

    added vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  pages: {
-    entry: 'src/index/main.js'
-  }
+  transpileDependencies: true
 });
