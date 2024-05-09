3204f7ee7544df3552516f17c44e070acb77577f
Andreas Axelsson <pererikandreas@gmail.com>
2024-05-09 19:11:29 +0200

    added cookie yes

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,51 +3,61 @@
     <h2>OM FÖRENINGEN</h2>   
     <div class="cards"> 
     <div class="card">
+      <div class="info-picture-container">
+        <img :src="InfoPicture" class="info-picture" alt="Kassi playing at Captain Cook" />
+
+      </div>
+      <div>
 <p>
-        Stuck On är samlingsplatsen för musikälskare i Östersund. Med
-        över 100 framgångsrika arrangemang under våra vingar har vi etablerat
-        oss som en central kraft inom den lokala musikscenen.
+        Stuck On är en samlingsplats för musikälskare i Östersund. Med
+        över 100 framgångsrika arrangemang i ryggen har vi etablerat
+        oss som en naturlig del av den lokala musikscenen.
       </p>
-    </div>
-    <div class="card">
+
       <p>
         Vår förening grundades med visionen att skapa en gemenskap för
         likasinnade som delar kärleken till musik och samtidigt göra staden mer levande
         genom att arrangera evenemang på flera scener.
       </p>
-    </div>
-    <div class="card">
+
       <p>
-        Våra arrangemang spänner över olika genrer och stilar för att tillgodose
-        den breda smaken hos vårt publikspektrum. Från intima konserter till
-        livliga rockkonserter strävar vi alltid efter att erbjuda en
-        upplevelse.
+        Våra arrangemang rör sig över olika genrer och stilar allt från intima akustiska konserter till
+        rockkonserter och ofta med lokala förband. Genom samarbeten med talangfulla musiker och ett nära sammarbete med studieförbundet Sensus strävar vi efter att bygga upp en blomstrande
+        lokal musikgemenskap.
       </p>
     </div>
-    <div class="card">
-      <p>
-        Vi är stolta över vårt engagemang för att stödja och främja lokala
-        talanger. Genom samarbeten med talangfulla musiker och studieförbundet Sensus strävar vi efter att bygga upp en blomstrande
-        musikgemenskap.
-      </p>
-    </div>
+  </div>
   </div>  
   </div>
 </template>
 
 <script>
+import InfoPicture from '../assets/photophonics-infopicture.jpg';
 export default {
   name: "Info",
+  data() {
+    return {
+      InfoPicture
+    };
+  }
 };
 </script>
 
 <style scoped>
+.info-picture {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  border-radius: 50%;
+}
+.info-picture-container {
+
+}
 .info-container {
   display: flex;
   flex-direction: column;
   max-width: 923px;
   margin: 0 auto 180px auto;
-  padding: 20px;
 }
 
 .cards {
@@ -60,10 +70,15 @@ export default {
 
 .card {
   background: #FAFAFA;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
   box-shadow: 0 2px 5px rgba(0,0,0,0.1);
   padding: 20px;
   border-radius: 8px;
-  transition: transform 0.3s ease-in-out;
+  text-align: left;
 }
 
 .card:hover {
@@ -75,5 +90,19 @@ h2 {
   color: var(--primary-color);
   margin-bottom: 10px;
   text-align: left;
+}
+
+@media (min-width: 700px) {
+  .card {
+
+  display: flex;
+  flex-direction: row;
+  
+}
+.info-picture {
+  width: 250px;
+  height: auto;
+  object-fit: cover;
+}
 }
 </style>
