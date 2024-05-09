55ff0ea158a57bc25563e1ed5c3d5e97b5531517
Andreas Axelsson <pererikandreas@gmail.com>
2024-05-09 11:48:33 +0200

    added to instasection

--- src/components/Instagram.vue
+++ src/components/Instagram.vue
@@ -101,7 +101,7 @@ export default {
   /* Add more styles for your paragraph */
 }
 
-/* @media (min-width: 700px) {
+@media (min-width: 700px) {
 .instagram-container {
   width: 92%;
   display: flex;
@@ -127,7 +127,7 @@ border-radius: 8px;
 padding: 20px 24px 20px 24px;
 flex-direction: row;
 }
-} */
+}
 
 @media (min-width: 1000px) {
 .instagram-post-container {
