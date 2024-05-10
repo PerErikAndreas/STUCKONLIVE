8935e09de07e97280c3eb0a213e876e692063eb7
Andreas Axelsson <pererikandreas@gmail.com>
2024-05-10 10:39:12 +0200

    config

--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,4 @@
 module.exports = {
-    // Base URL
-    publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
-  
-    // Output Directory
-    outputDir: 'dist',
-  
-    // Disable source maps in production for enhanced security
+    // Disable production source maps
     productionSourceMap: false,
-  
-    // Extend webpack configuration
-    configureWebpack: {
-      performance: {
-        hints: false
-      },
-      optimization: {
-        splitChunks: {
-          chunks: 'all'
-        }
-      }
-    },
-  
-    // Dev Server Configuration
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://example.com/api',
-          changeOrigin: true,
-          pathRewrite: {
-            '^/api': ''
-          }
-        }
-      }
-    }
   };
-  
