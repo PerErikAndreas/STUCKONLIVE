02585c2c17c61a0eea948b008ddcaa1f49adafea
Andreas Axelsson <pererikandreas@gmail.com>
2024-05-26 08:28:49 +0200

    added loop to upcounter

--- src/components/Trustbanner.vue
+++ src/components/Trustbanner.vue
@@ -1,34 +1,74 @@
 <template>
   <div class="trustbanner-container">
     <div class="icon-container">
-      <img class="stuckon-logo" :src="musicNoteIcon" alt="Music Note Icon" aria-label="100+ Spelningar" loading="lazy">
+      <img
+        class="stuckon-logo"
+        :src="musicNoteIcon"
+        alt="Music Note Icon"
+        aria-label="100+ Spelningar"
+        loading="lazy"
+      />
       <div class="icon-container-title">
-        <count-up :end-val="100"><template v-slot:suffix>+</template>
+        <count-up
+          :end-val="100"
+          :enable-scroll-spy="true"
+          :delay="4"
+          :duration="2.5"
+          :loop="2"
+          ><template v-slot:suffix>+</template>
         </count-up>
         EVENTS
       </div>
     </div>
     <div class="icon-container">
-      <img class="stuckon-logo" :src="visitorsIcon" alt="Visitors Icon" aria-label="10,000+ Besökare" loading="lazy">
+      <img
+        class="stuckon-logo"
+        :src="visitorsIcon"
+        alt="Visitors Icon"
+        aria-label="10,000+ Besökare"
+        loading="lazy"
+      />
       <div class="icon-container-title">
-        <count-up :end-val="10000"><template v-slot:suffix>+</template>
+        <count-up
+          :end-val="10000"
+          :enable-scroll-spy="true"
+          :delay="4"
+          :duration="2.5"
+          :loop="2"
+          ><template v-slot:suffix>+</template>
         </count-up>
         BESÖKARE
       </div>
     </div>
     <div class="icon-container">
-      <img class="stuckon-logo" :src="locationIcon" alt="Location Icon" aria-label="4 Scener" loading="lazy">
+      <img
+        class="stuckon-logo"
+        :src="locationIcon"
+        alt="Location Icon"
+        aria-label="4 Scener"
+        loading="lazy"
+      />
       <div class="icon-container-title">
-        <count-up :end-val="4"></count-up>
+        <count-up
+          :end-val="4"
+          :enable-scroll-spy="true"
+          :delay="4"
+          :duration="2.5"
+          :loop="2"
+        ></count-up>
         SCENER
       </div>
     </div>
-    <img class="graphic-quarter2" src="../assets/animationquarter.png" alt="Loading animation" />
+    <img
+      class="graphic-quarter2"
+      src="../assets/animationquarter.png"
+      alt="Loading animation"
+    />
   </div>
 </template>
 
 <script>
-import CountUp from 'vue-countup-v3'
+import CountUp from "vue-countup-v3";
 
 export default {
   name: "Trustbanner",
@@ -37,9 +77,9 @@ export default {
   },
   data() {
     return {
-      musicNoteIcon: require('../assets/musicnote.png'),
-      visitorsIcon: require('../assets/visitors-icon.png'),
-      locationIcon: require('../assets/location.png'),
+      musicNoteIcon: require("../assets/musicnote.png"),
+      visitorsIcon: require("../assets/visitors-icon.png"),
+      locationIcon: require("../assets/location.png"),
     };
   },
 };
@@ -61,9 +101,9 @@ export default {
 
 .graphic-quarter2 {
   position: absolute;
-height: 35%;  
-bottom: 0;
-  left:-5px;
+  height: 35%;
+  bottom: 0;
+  left: -5px;
   border-radius: 0 100% 0 0;
 }
 
@@ -91,13 +131,12 @@ bottom: 0;
   }
 
   .icon-container-title {
-  margin-top: 10px;
-}
+    margin-top: 10px;
+  }
 
   .graphic-quarter2 {
-height: 65%;  
-
-}
+    height: 65%;
+  }
 }
 @media (min-width: 1400px) {
   .trustbanner-container {
