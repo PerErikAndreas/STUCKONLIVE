6694358cd843f8c3c4998e816fe73135826fc626
Andreas Axelsson <pererikandreas@gmail.com>
2025-11-17 23:00:11 +0100

    added news and made poster

--- src/components/UntilNews.vue
+++ src/components/UntilNews.vue
@@ -8,7 +8,7 @@
           <div class="card" v-if="currentNews">
             <!-- Event Image -->
             <div class="card-image">
-              <img src="@/assets/posters/JerryLeger2025.jpg" alt="Event" />
+              <img src="@/assets/posters/JesperLindell2.jpg" alt="Event" />
             </div>
 
             <!-- Event Text -->
